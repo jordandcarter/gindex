26914430e03135b487593db572ee08056e9dd040
Jordan <jordan@yoda-vb.(none)>
2010-04-05 20:42:35 +1200

    added styles

--- app/models/website.rb
+++ app/models/website.rb
@@ -9,13 +9,16 @@ class Website < ActiveRecord::Base
   named_scope :to_date, lambda { |date| {:conditions => ["websites.created_at <= ?", date] } }
   
   def count
-    counts << WebsiteIndexCount.new(:count => Counter.grab(url).gsub(",","").to_i)
+    c= WebsiteIndexCount.new(:count => Counter.grab(url).gsub(",","").to_i)
+    counts << c
+    puts "#{c.website.url}:#{c.count}" if RAILS_ENV=="development"
   end
   
   def self.count_all
     Website.all.each do |website|
       website.count
-      sleep 30+rand(30)
+      delay = RAILS_ENV=="development" ? 5 : 30
+      sleep delay+rand(delay)
     end
   end
   
--- lib/counter.rb
+++ lib/counter.rb
@@ -28,16 +28,14 @@ class Counter
         while !recieved && try_count < 5
           begin #catch timeouts
             page_response = open(url, 
-            "User-Agent" => "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.5; en-US; rv:1.9.0.4) Gecko/2008102920 Firefox/3.0.4",
+            "User-Agent" => random_user_agent,
             "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
             "Accept-Language" => "en-us,en;q=0.5",
             "Accept-Charset" => "ISO-8859-1,utf-8;q=0.7,*;q=0.7",
             "Keep-Alive" => "300",
             "Connection" => "keep-alive",
-            "Referer" => "http://www.google.co.nz/search?num=100&hl=en&btnG=Search&meta=",
-            "X-Moz" => "prefetch")#,
-            #"Cookie" => 'SS=Q0=cHJvamVjdCB4; PREF=ID=208490125f4598d3:TM=1226974432:LM=1226974432:S=C6DNMw-2IjAEIM1l; NID=17=X3C1qPj4bGtlrxdH0fxbb8wwwHTGcfaC63fndLGwm3hj_A18nTXRNyCEbQ2dFPLXf6fob2a0rHNs48l8wXH_mJJvs8MlYPgB0ZSfojLKbTZt6s4olZK5HEgJrQ9GNRDd')
-          
+            "Referer" => "http://www.google.co.nz/search?&hl=en&btnG=Search&meta="
+            )
           recieved = true
           rescue
           try_count += 1
@@ -78,5 +76,21 @@ class Counter
   def self.clean_keywords(keywords)
     keywords.gsub!(/[^&A-Za-z01-9 ]\+?/, "") #remove everything but those allowed in the keyword search
     keywords.gsub(' ', '+') #replace spaces with + (for googlesearch url)
+  end
+  
+  def self.random_user_agent
+     agents = ["Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6",
+     "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)",
+     "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)",
+     "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)",
+     "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT 5.1; .NET CLR 1.1.4322)",
+     "Opera/9.20 (Windows NT 6.0; U; en)",
+     "Opera/9.00 (Windows NT 5.1; U; en)",
+     "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; en) Opera 8.50",
+     "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; en) Opera 8.0",
+     "Mozilla/4.0 (compatible; MSIE 6.0; MSIE 5.5; Windows NT 5.1) Opera 7.02 [en]",
+     "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.5) Gecko/20060127 Netscape/8.1",
+     "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.5; en-US; rv:1.9.0.4) Gecko/2008102920 Firefox/3.0.4"]
+     agents[rand(agents.length)]
   end
 end
